4f265138008ec79a335689f390932b1c2d76a271
cphillips16 <caidi.gillett@gmail.com>
2017-04-19 15:49:14 -0500

    RegistrationInfoData.php and RegistrationPage.php are finished.

--- FreshSelectPHPCode/RegistrationInfoData.php
+++ FreshSelectPHPCode/RegistrationInfoData.php
@@ -2,33 +2,84 @@
 include ("DatabaseConnection.php")
 ?>
 <html>
+<!--- Coded by Caidi --->
 <body>
 
-Welcome <?php echo $_GET["firstName"]; ?><br>
-
 <?php
+$tempPrinId;
+$tempGender;
+$tempFreshHouse;
+
 $prinId = $_GET["prinId"];
 $firstName = $_GET["firstName"];
 $lastName = $_GET["lastName"];
 $newUser = $_GET["newUser"];
 $newPwd = $_GET["newPwd"];
-$confirmPwd = $_GET["ConfirmPwd"];
+$confirmPwd = $_GET["confirmPwd"];
+$gender = $_GET["gender"];
+$freshHouse = $_GET['freshHouse'];
 
-if ($newPwd == $confirmPwd) {
-    $sql = "INSERT INTO Person (prinId, firstName, lastName, login, password, permission) 
-    VALUES ('$prinId', '$firstName', '$lastName', '$newUser', '$password', DEFAULT )";
+$tempPrinId = substr($prinId, -9, 3);
 
-    if(mysqli_query($db, $sql)){
-        echo "New User added successfully.";
-    } else{
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
+if (strtoupper($gender) == 'MALE')
+    $tempGender = strtoupper(substr($gender, -3, 1));
+else if (strtoupper($gender) == 'FEMALE')
+    $tempGender = strtoupper(substr($gender, -6, 1));
+
+if (strtoupper($freshHouse) == 'ANDERSON')
+    $tempFreshHouse = strtoupper($freshHouse);
+else if (strtoupper($freshHouse) == 'RACKHAM')
+    $tempFreshHouse = strtoupper($freshHouse);
+
+if (empty($prinId))
+    echo "Please enter your Prin ID.";
+else if ($tempPrinId != "P01")
+    echo "Please enter a valid P01 id";
+else if (empty($firstName))
+    echo "Please enter your First Name.";
+else if (empty($lastName))
+    echo "Please enter your Last Name.";
+else if (empty($newUser))
+    echo "Please enter your Username.";
+else if (empty($newPwd))
+    echo "Please enter your password.";
+else if (empty($confirmPwd))
+    echo "Please confirm your password.";
+else if (empty($gender))
+    echo "Please enter your gender.";
+else if (empty($tempGender))
+    echo "Please enter either female or male.";
+else if (empty($freshHouse))
+    echo "Please enter your freshman house.";
+else if (empty($tempFreshHouse))
+    echo "Please enter either Anderson or Rackham.";
+else {
+    if ($newPwd == $confirmPwd) {
+        $sql = "INSERT INTO Person (prinId, firstName, lastName, login, password, permission) 
+           VALUES ('$prinId', '$firstName', '$lastName', '$newUser', PASSWORD('$confirmPwd'), DEFAULT )";
+
+        if (mysqli_query($db, $sql)) {
+            $sql = "INSERT INTO Student (prinId, gender, freshHouse, upperHouse, preference) 
+           VALUES ('$prinId', '$tempGender', '$tempFreshHouse', DEFAULT , DEFAULT )";
+
+
+            if (mysqli_query($db, $sql)) {
+                echo "New User added successfully.";
+                header('Refresh: 2; URL = LoginPage.php');
+                exit();
+            }
+            else {
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
+            }
+
+        } else {
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
+        }
+    } else {
+        echo "Passwords did not match.";
     }
 }
-else {
-   echo "Passwords did not match.";
-}
-
 ?>
-<INPUT TYPE = "button" VALUE = "Back" onClick = "history.go(-1);"
+<br><INPUT TYPE = "button" VALUE = "Back" onClick = "history.go(-1);"/>
 </body>
 </html>
--- FreshSelectPHPCode/RegistrationPage.php
+++ FreshSelectPHPCode/RegistrationPage.php
@@ -1,9 +1,15 @@
-<html
+<?php
+include ("DatabaseConnection.php")
+?>
+
+<html>
 
 <head>
     <title>Registration Page</title>
 </head>
 <style>
+    .error {
+        color: red;}
     body {
         padding-top: 150px;
         padding-bottom: 90px;
@@ -12,17 +18,60 @@
 
 <body>
 
-<?php echo '<center><h1>Registration</h1></center>';?>
+<?php echo '<center><h1>Registration</h1></center>'; ?>
 
-<form action = "RegistrationInfoData.php" method = "get">
-    <center>Prin ID: <input type = "text" name = "prinId"><br><br></center>
-    <center>First Name: <input type = "text" name = "firstName"><br><br></center>
-    <center>Last Name: <input type = "text" name = "lastName"><br><br></center>
-    <center>Username:   <input type = "text" name = "newUser"><br><br></center>
-    <center>Password:   <input type = "text" name = "newPwd"><br><br></center>
-    <center>Confirm Password:   <input type = "text" name = "ConfirmPwd"><br><br></center>
-    <center><input type = "Submit" </center>
+<p><center><span class = "error" > * required field. </span></p></center>
+<form  method = "GET" action="RegistrationInfoData.php">
+    <center>Prin ID: <input type = "text" name = "prinId">
+        <span
+                class="error">*
+        </span>
+        <br><br>
+    </center>
+    <center>First Name: <input type = "text" name = "firstName">
+        <span
+                class="error">*
+        </span>
+        <br><br>
+    </center>
+    <center>Last Name: <input type = "text" name = "lastName">
+        <span
+                class="error">*
+        </span>
+        <br><br>
+    </center>
+    <center>Gender: <input type = "text" name = "gender">
+        <span
+            class="error">*
+        </span>
+        <br><br>
+    </center>
+    <center>Freshman House:<input type = "text" name = "freshHouse">
+        <span
+            class="error">*
+        </span>
+            <br><br>
+    </center>
+    <center>
+        Username:   <input type = "text" name = "newUser">
+        <span
+                class="error">*
+        </span>
+        <br><br>
+    </center>
+    <center>Password:   <input type = "password" name = "newPwd">
+        <span
+                class="error">*
+        </span>
+        <br><br>
+    </center>
+    <center>Confirm Password:   <input type = "password" name = "confirmPwd">
+        <span
+                class="error">*
+        </span>
+        <br><br>
+    </center>
+    <center><input type = "submit" name = "submit" value = "Submit" </center>
 </form>
 </body>
 </html>
-
